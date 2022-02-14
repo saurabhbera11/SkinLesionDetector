8317fdb0efcee705498ceb2433bc0ec438f3c54c
Grp-25SkinLesion <98631391+Grp-25SkinLesion@users.noreply.github.com>
2022-02-14 20:50:04 +0530

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -33,6 +33,27 @@ function analyze() {
     }
     el("analyze-button").innerHTML = "Analyze";
   };
+  
+  function severity(){
+    var uploadFiles = el("file-input").files;
+    if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
+
+    el("analyze-button").innerHTML = "Analyzing...";
+    var xhr = new XMLHttpRequest();
+    var loc = window.location;
+    xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
+    true);
+    xhr.onerror = function() {
+    alert(xhr.responseText);
+  };
+  xhr.onload = function(e) {
+    if (this.readyState === 4) {
+      var response = JSON.parse(e.target.responseText);
+      el("result-label").innerHTML = `Result = ${response["result"]}`;
+    }
+    el("analyze-button").innerHTML = "Analyze";
+    
+  }
 
   var fileData = new FormData();
   fileData.append("file", uploadFiles[0]);
