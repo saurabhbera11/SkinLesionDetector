93f48bda847de48fea8336e11174faeac4b98f35
Grp-25SkinLesion <98631391+Grp-25SkinLesion@users.noreply.github.com>
2022-02-16 19:07:48 +0530

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -34,27 +34,6 @@ function analyze() {
     el("analyze-button").innerHTML = "Analyze";
   };
   
-  function severity(){
-    var uploadFiles = el("file-input").files;
-    if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
-
-    el("severity-button").innerHTML = "Analyzing...";
-    var xhr = new XMLHttpRequest();
-    var loc = window.location;
-    xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/severity`,
-    true);
-    xhr.onerror = function() {
-    alert(xhr.responseText);
-  };
-  xhr.onload = function(e) {
-    if (this.readyState === 4) {
-      var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `The result is = ${response["result"]} (Melanoma) and it is a malignant lesion';
-    }
-    el("analyze-button").innerHTML = "Analyze";
-    
-  }
-
   var fileData = new FormData();
   fileData.append("file", uploadFiles[0]);
   xhr.send(fileData);
