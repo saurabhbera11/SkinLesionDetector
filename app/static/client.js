202c28521f053a09a94310f6230e34850dc1de23
Grp-25SkinLesion <98631391+Grp-25SkinLesion@users.noreply.github.com>
2022-02-17 10:18:48 +0530

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -29,7 +29,8 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `Result = ${response["result"]} and the type is melanoma`;
+      var result='${response["result"]}'
+      el("result-label").innerHTML="There is a possibility of skin lesion being Melanoma and it is malignant in nature."
     }
     el("analyze-button").innerHTML = "Analyze";
   };
